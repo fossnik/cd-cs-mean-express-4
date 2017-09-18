1807441d2ae35eea57c673d50074c71b412875a5
Zack Hartmann <zackhartmann@gmail.com>
2017-09-18 15:19:18 -0400

    Adding links to Blocks

--- public/client.js
+++ public/client.js
@@ -17,6 +17,20 @@ $(function(){
 		var cityData = form.serialize();
 		$.ajax({
 			type: 'POST', url: '/cities', data: blockData
-		}).done(function(cityName))
+		}).done(function(cityName){
+			// appendToList function expects and array of cities
+			// cityName is array with the new city as its single argument.
+			appendToList([cityName]);
+				var list = [];
+				var content, city;
+				// builds out html block for each city - serial
+				for(var i in cities){
+					city = cities[i];
+					content = '<a href="/blocks/'+city+'">'+city+'</a>';
+					list.push($('<li>', { html: content}));
+				}
+			// cleans up text input fields
+			form.trigger('reset');
+		});
 	});
 });
