6456c1acc1bf4861aaeb17f40299b302a510e722
Zack Hartmann <zackhartmann@gmail.com>
2017-09-18 15:07:56 -0400

    Submitting the form with JavaScript (PDF77)

--- public/client.js
+++ public/client.js
@@ -7,3 +7,16 @@ $(function(){
 		$('.cities').html(html);
 	});
 });
+
+$(function(){
+	$.get('/cities', appendToList);
+	$('form').on('submit', function(event) {
+		event.preventDefault();
+		var form = $(this);
+		// serialize transforms form data to URL-encoded notation.
+		var cityData = form.serialize();
+		$.ajax({
+			type: 'POST', url: '/cities', data: blockData
+		}).done(function(cityName))
+	});
+});
