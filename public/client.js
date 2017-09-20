fd91693aae2350653c530eeb108dbccae35110df
Zack Hartmann <zackhartmann@gmail.com>
2017-09-20 17:10:37 -0400

    more

--- public/client.js
+++ public/client.js
@@ -1,14 +1,14 @@
 $(function(){
 	$.get('/cities', appendToList);
 
-	function appendToList(someCities) {
+	function appendToList(cities) {
 		var html = "";
 		// have to use Object.keys so forEach can have an array, bc objecs NG
-		Object.keys(someCities).forEach(function(city, index){
+		Object.keys(cities).forEach(function(city, index){
 			html += "<tr>";
-				html += "<td>"+city+",&nbsp;<em>"+Object.values(someCities)[index]+"</em></td>";
+				html += "<td>"+city+",&nbsp;<em>"+Object.values(cities)[index]+"</em></td>";
 				// set city name to data-city attribute, which will be used to compose AJAX request.
-				html += "<td><button data-city='"+city+"'>REMOVE</button></td>";
+				html += "<td><a href='#' data-city='"+city+"' href='/cities/"+city+"'>REMOVE</a></td>";
 			html += "</tr>";
 		})
 		$('.cities-list').html(html);
@@ -27,22 +27,14 @@ $(function(){
 		}).success(function(cities){
 			// appendToList function expects an array of cities
 			// citySubmitted is array with the new city as its single argument.
-			// appendToList(cities);
-			var list = [];
-			var content;
-			// creates an array of objects (each city an object)
-			Object.keys(cities).forEach(function(city){
-				content = '<a href="/cities/'+city+'">'+city+'</a>';
-				list.push($('<tr>', { html:content }));
-			})
-			$('.cities-list').html(list);
+			appendToList(cities);
 			// clears input fields after submit
 			form.trigger('reset');
 		});
 	});
 	// this is the implementation of the delete city functionality.
 	// listens for a click event on any button with a data-city attribute.
-	$('.cities-list').on('click', 'button[data-city]', function(event){
+	$('.cities-list').on('click', 'a[data-city]', function(event){
 		if (!confirm('Are you sure ?')) {	return false;	}
 		// the clicked on element that triggered the click event (target)
 		// $(event) wraps target into a jQuery object to make it easier to work with.
@@ -51,7 +43,7 @@ $(function(){
 		$.ajax({
 			type: 'DELETE', url: '/cities/' + target.data('city')
 		}).done(function() {
-			// removes li element containing the link
+			// removes <tr> table element containing the link
 			target.parents('tr').remove();
 		});
 	});
